2052e3dbea4035bd602c9e1b6e2a823125a1b4d3
mrnegativex <rashadul99924@gmail.com>
2022-05-21 19:00:06 +0600

    error

--- src/Pages/Genarel/Home/Banner.js
+++ src/Pages/Genarel/Home/Banner.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Chair from '../../../assets/images/chair.png'
+import chairBg from '../../../assets/images/bg.png'
 
 const Banner = () => {
+    const backgroundImgStyle = {
+        backgroundImage: `url("${chairBg}")`, 
+        backgroundSize: 'cover',
+        backgroundPosition: 'left bottom'
+    }
     return (
-        <div>
-            <div class="hero min-h-screen bg-base-200">
-  <div class="hero-content flex-col lg:flex-row-reverse">
+        <div className='  ' style={backgroundImgStyle}>
+            <div class="hero mx-auto container min-h-screen" >
+  <div class="hero-content flex-col lg:flex-row-reverse" >
     <img src={Chair} className="max-w-sm rounded-lg shadow-2xl" alt=''/>
     <div>
-      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
       <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button class="btn btn-primary">Get Started</button>
+      <button class="btn bg-gradient-to-r from-primary to-secondary border-0 font-bold text-white">Get Started</button>
     </div>
   </div>
 </div>
--- src/Pages/Genarel/Home/Home.js
+++ src/Pages/Genarel/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Info from './InfoCard'
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Info></Info>
         </div>
     );
 };
